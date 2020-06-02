e3ba5939fb727466a7c6198a231fb6ba59bb1848
Dtesch9 <alucard.s8123@gmail.com>
2020-06-02 13:40:27 -0300

    init

---
+++ backend/src/database/connection.ts
@@ -0,0 +1,11 @@
+import knex from 'knex';
+import { resolve } from 'path';
+
+const connection = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: resolve(__dirname, 'src', 'database', 'database.sqlite'),
+  },
+});
+
+export default connection;
---
+++ backend/src/routes/usersRouter.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const usersRouter = Router();
+
+usersRouter.get('/', (request, response) => response.json({ message: 'ok' }));
+
+export default usersRouter;
